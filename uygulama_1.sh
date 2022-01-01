188a67a448a880b41669fd4be97216698097d99c
alperentvs <alperentavas45@gmail.com>
2022-01-01 17:34:59 +0300

    uygulama_1.sh eklendi

---
+++ uygulama_1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "$0 programini baslattiniz"
+echo "Ilk arguman=$1"
+echo "Ikinci arguman=$1"
+echo "Ucuncu arguman=$1"
+echo "Butun argumanlar=$*"
+echo "Arguman sayisi=$#"
+shift       # Girilen ifadeleri bir birim sola kaydirir. Bu sonuctan sonra "pardus" ifadesi gorulmeyecek.
+echo "$0 programini baslattiniz"
+echo "Ilk arguman=$1"
+echo "Ikinci arguman=$1"
+echo "Ucuncu arguman=$1"
+echo "Butun argumanlar=$*"
+echo "Arguman sayisi=$#"
